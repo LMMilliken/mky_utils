f18fb0ce9e54bd6655967cae40deb76f42036e04
LMMilliken <louis@djmilliken.co.uk>
2024-10-23 13:48:43 +0900

    feat: name generation

--- mky_utils/utils.py
+++ mky_utils/utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 import requests
 
@@ -12,3 +13,20 @@ def notify(message: str, stdout=True):
         pass
     if stdout:
         print(message)
+
+
+def generate_name() -> str:
+    names = []
+
+    for generation in range(1, 5):
+        response = requests.get(f"https://pokeapi.co/api/v2/generation/{generation}/")
+        generation_data = response.json()
+        for name in generation_data["pokemon_species"]:
+            names.append(name["name"])
+
+    response = requests.get(
+        "https://raw.githubusercontent.com/dariusk/corpora/master/data/words/adjs.json"
+    )
+    adjectives_data = response.json()
+    adjectives_list = adjectives_data["adjs"]
+    return f"{random.choice(adjectives_list)}-{random.choice(names)}"
